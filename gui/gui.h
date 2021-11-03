80a478a944eb6784176a2f70ad4a7efc449d4e7e
Vachagan Gratian <vgratian@utopianlab.am>
2021-11-03 13:37:06 +0400

    prototypes of c functions

---
+++ gui/gui.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+
+// public functions
+int find_wondow(const char*, int);
+
+unsigned long* scan_pixels(int*);
+
+void fake_key_event(const char*);
+
+// private functions
+static unsigned long _find_window(unsigned long, const char*);
+static void _fix_coordinates();
+
